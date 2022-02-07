b306cd4ab258cf3420643c31085a07527d5f8d02
francois18 <79960710+Francois18@users.noreply.github.com>
2022-02-07 20:38:15 +0300

    updates source

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
+import React from "react";
+import Home from "./Screens/Home";
+import Login from "./Screens/Auth/Login";
+import Form from "./Screens/Entity/Form";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import List from "./Screens/Entity/List";
+
 function App() {
   return (
-    <div className="mt-10 text-center text-white font-bold">
-      <h1 className="text-4xl pb-6" >Voters</h1>
-    <p class="capitalize "> Online Voting Made Easy</p>
-    </div>
+    <Router>
+      <div className="flex flex-col  justify-center items-center min-h-screen py-20 bg-blue-500">
+          <Routes>
+              <Route exact path="/" element={<Home/>}/>
+              <Route exact path="/login" element={<Login/>}/>
+              <Route exact path="/form" element={<Form/>}/>
+              <Route exact path="/list" element={<List/>}/>
+
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
